fee1981ca21dc1b5cf66a79d94525ecff0013131
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-09 15:03:56 +0100

    font

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,16 @@
 import "./globals.css";
 import Header from "./components/Header/Header";
+import { Poppins } from "next/font/google";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
-      <body className={`antialiased bg-bg-main`}>
+      <body className={`${poppins.className} antialiased bg-bg-main`}>
         <Header />
         <main>{children}</main>
       </body>
