58e1160419ddc9d46f6f9e1b840ce453ba865000
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-09 12:31:23 +0100

    test

--- app/page.js
+++ app/page.js
@@ -1,16 +1,26 @@
-import About from "./homePage/sections/About";
-import Hero from "./homePage/sections/Hero";
-import Projects from "./homePage/sections/Projects";
+"use client";
 
-export const metadata = {
-  title: "Sadowski Studio",
-  description: "Strona portfolio Sadowski Studio",
-};
+import dynamic from "next/dynamic";
+import Hero from "./homePage/sections/Hero";
+
+// Lazy load sekcji poza foldem
+const About = dynamic(() => import("./homePage/sections/About"), {
+  ssr: false,
+  loading: () => <p>Ładowanie sekcji About...</p>,
+});
+
+const Projects = dynamic(() => import("./homePage/sections/Projects"), {
+  ssr: false,
+  loading: () => <p>Ładowanie sekcji Projects...</p>,
+});
 
 export default function Home() {
   return (
     <>
+      {/* Hero widoczny od razu */}
       <Hero />
+
+      {/* Pozostałe sekcje ładowane dynamicznie */}
       <About />
       <Projects />
     </>
