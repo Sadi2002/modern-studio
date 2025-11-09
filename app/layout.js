188d06acdc8a4312506a2bce492662c317b5ccd9
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-09 14:32:29 +0100

    test

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header";
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
-      <body className={` antialiased bg-bg-main`}>
+      <body className={`font-[Poppins] antialiased bg-bg-main`}>
         <Header />
         <main>{children}</main>
       </body>
