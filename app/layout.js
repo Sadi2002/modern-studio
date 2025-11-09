6158bdec1124a33c21316b2b6a5af60c67c9df35
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-09 13:41:23 +0100

    test

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header/Header";
-import SmoothScroll from "./components/SmothScroll";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -16,7 +15,7 @@ export default function RootLayout({ children }) {
         className={`${poppins.className} antialiased h-[10009px] bg-bg-main`}
       >
         <Header />
-        <SmoothScroll />
+
         <main>{children}</main>
       </body>
     </html>
