ebc336232885944c9867c2c9b1aa7be303079fb8
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-10 23:56:05 +0100

    test

--- app/homePage/sections/Projects.js
+++ app/homePage/sections/Projects.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import Project1 from "../../../public/project1.webp";
-import Project2 from "../../../public/project2.webp";
-import Project3 from "../../../public/project3.webp";
-import Project4 from "../../../public/project4.webp";
+import Project1 from "../../../public/1.webp";
+import Project2 from "../../../public/1.webp";
+import Project3 from "../../../public/1.webp";
+import Project4 from "../../../public/1.webp";
 import ArrowWhite from "../../../public/arrow-right-white.png";
 // import { useMediaQuery } from "usehooks-ts";
 
