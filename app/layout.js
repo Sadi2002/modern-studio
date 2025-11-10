201879711971cac95730b40bc0558714c12358bb
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-10 23:58:25 +0100

    test

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,17 @@
 import "./globals.css";
 import Header from "./components/Header/Header";
+import { Poppins } from "next/font/google";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500"],
+  display: "swap",
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
-      <body className={` bg-bg-main`}>
+      <body className={`${poppins.className} h-[1000px] bg-bg-main`}>
         <Header />
         <main>{children}</main>
       </body>
