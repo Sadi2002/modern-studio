7060c1391ad4d57265d9d288559c98694918878e
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-11 22:33:56 +0100

    test

--- app/homePage/sections/About.js
+++ app/homePage/sections/About.js
@@ -1,50 +1,60 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 
 export default function About() {
   const [loaded, setLoaded] = useState(false);
 
+  useEffect(() => {
+    const img = new window.Image();
+    img.src = "/about-large.jpg";
+    img.onload = () => {
+      setLoaded(true);
+    };
+  }, []);
+
   return (
     <section className="pt-about-section-padding-top-mobile flex flex-col gap-about-section-gap-mobile xl:gap-about-section-gap-laptop xl:pt-about-section-padding-top-laptop 2xl:gap-about-section-gap-desktop mb-about-section-margin-bottom xl:mb-[150px]">
-      <div className="mx-margin-mobile lg:flex md:mx-tablet lg:mx-small-laptop lg:justify-between xl:justify-between 2xl:mx-desktop">
-        <h2 className="text-about-title-size-mobile leading-about-title-line-height-mobile font-medium mb-about-title-margin-bottom max-w-about-title-max-width-mobile lg:text-about-title-size-laptop lg:leading-about-title-line-height-laptop lg:max-w-about-title-max-width-laptop lg:w-about-title-width-laptop 2xl:max-w-about-title-max-width-desktop xl:font-normal">
+      <div className=" mx-margin-mobile lg:flex md:mx-tablet lg:mx-small-laptop lg:justify-between xl:justify-between 2xl:mx-desktop">
+        <h2 className="text-about-title-size-mobile leading-about-title-line-height-mobile font-medium mb-about-title-margin-bottom max-w-about-title-max-width-mobile lg:text-about-title-size-laptop lg:leading-about-title-line-height-laptop lg:max-w-about-title-max-width-laptop lg:w-about-title-width-laptop 2xl:max-w-about-title-max-width-desktop  xl:font-normal">
           Witamy w Visual Studio Twoje eleganckie
         </h2>
         <div className="flex flex-col items-end lg:items-start lg:w-about-text-width-laptop">
-          <div className="flex flex-col gap-about-text-box-gap font-light-font-weight mb-about-text-box-margin-bottom items-start text-about-text-box-size leading-[clamp(0.75rem,10vw, 1.5rem)] w-full-width">
-            <p className="lg:max-w-about-text-max-width-small-laptop 2xl:max-w-full-width">
+          <div
+            className="flex flex-col gap-about-text-box-gap font-light-font-weight mb-about-text-box-margin-bottom items-start text-about-text-box-size
+          leading-[clamp(0.75rem,10vw, 1.5rem)] w-full-width"
+          >
+            <p className=" lg:max-w-about-text-max-width-small-laptop 2xl:max-w-full-width">
               Projektując przestrzeń, poszukuję harmonii między światłem a
               materią. Perfekcja detalu prowadzi do ponadczasowej formy, która
               inspiruje użytkowników.
             </p>
-            <p className="lg:max-w-about-text-max-width-small-laptop 2xl:max-w-full-width">
+            <p className="  lg:max-w-about-text-max-width-small-laptop 2xl:max-w-full-width">
               Przestrzeń powinna oddychać światłem i funkcją. Projektując,
               poszukuję relacji między naturą a strukturą. Perspektywa
               użytkownika prowadzi każdy detal, czyniąc architekturę.
             </p>
           </div>
-          <button className="font-semibold-font-weight text-[clamp(0.75rem,3.5vw,1rem)] relative uppercase after:content-[''] after:bg-main-black after:absolute after:bottom-[-0.5px] after:left-0 after:w-full-width after:h-[1px]">
+          <button className="font-semibold-font-weight text-[clamp(0.75rem,3.5vw,1rem)] relative uppercase after:content-[''] after:bg-main-black after:absolute after:bottom-[-0.5px] after:left-0 after:w-full-width after:h-[1px] ">
             Więcej o nas
           </button>
         </div>
       </div>
-
       <div className="relative max-w-about-image-max-width-mobile xl:w-about-image-width-laptop aspect-about-image-aspect-ratio">
-        {/* Mały obrazek — widoczny od razu */}
         <Image
           src="/about-small.jpg"
           alt="pokój"
           fill
           className={`object-cover transition-opacity duration-700 ${
-            loaded ? "opacity-0" : "opacity-100"
+            loaded ? "opacity-0" : "opacity-50"
           }`}
-          style={{ filter: "blur(10px)" }} // opcjonalny efekt blur
           unoptimized
+          priority
         />
 
-        {/* Duży obrazek — fade-in po załadowaniu */}
+        {/* Duże zdjęcie — fade-in po załadowaniu */}
         <Image
           src="/about-large.jpg"
           alt="pokój"
@@ -52,7 +62,6 @@ export default function About() {
           className={`object-cover transition-opacity duration-700 ${
             loaded ? "opacity-100" : "opacity-0"
           }`}
-          onLoadingComplete={() => setLoaded(true)}
           unoptimized
         />
       </div>
