ab1a2bc83d2960cdb5d949dfd003b8fd6dd5e0a4
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-11 13:31:16 +0100

    test

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500"],
   display: "swap",
+  preload: true,
 });
 
 export default function RootLayout({ children }) {
