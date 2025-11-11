510f07826bb98b62055dae55ad94623f2a970d8d
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-11 19:54:41 +0100

    test

--- app/portfolio/PortfolioClient.js
+++ app/portfolio/PortfolioClient.js
@@ -25,7 +25,7 @@ function ProgressiveImage({ smallSrc, largeSrc, alt }) {
 
   return (
     <div className="relative w-full h-full overflow-hidden">
-      {/* Mały obraz – od razu widać */}
+      {/* Mały obraz – od razu widac */}
       <Image
         src={smallSrc}
         alt={alt}
