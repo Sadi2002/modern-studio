6d0cc218beafdba59a37e2779b8258d85e12073e
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-13 23:42:51 +0100

    t

--- app/homePage/sections/Hero.js
+++ app/homePage/sections/Hero.js
@@ -16,6 +16,8 @@ export default function Hero() {
           placeholder="blur"
           blurDataURL="/about-small.webp"
           className="object-cover"
+          sizes="100vw"
+          quality={85}
         />
 
         <div className="absolute inset-0 bg-[rgba(0,0,0,0.55)]"></div>
@@ -26,11 +28,13 @@ export default function Hero() {
           <h1 className="text-main-white text-hero-title-size-mobile leading-hero-title-line-height-mobile font-medium mb-hero-title-margin-bottom lg:text-hero-title-size-small-laptop lg:leading-hero-title-line-height-small-laptop xl:text-hero-title-size-laptop xl:font-normal-font-weight xl:leading-hero-title-line-height-laptop uppercase 2xl:leading-hero-title-line-height-desktop 2xl:text-hero-title-size-desktop max-w-hero-title-max-width-mobile lg:max-w-hero-title-max-width-small-laptop xl:max-w-hero-title-max-width-laptop 2xl:max-w-hero-title-max-width-desktop">
             OSIEDLE ZACISZE W Warszawie
           </h1>
+
           <p className="text-main-white font-light-font-weight mb-hero-text-margin-bottom-mobile text-hero-text-size-mobile leading-hero-text-line-height-mobile max-w-hero-text-max-width-mobile lg:max-w-hero-text-max-width-small-laptop min-[420px]:max-w-[360px] xl:max-w-hero-text-max-width-laptop xl:mb-hero-text-margin-bottom-laptop">
             Oferujemy doświadczenie wyrafinowanego komfortu, ponadczasowej
             elegancji i szczerej gościnności. Położona w romantycznym sercu
             Paryża.
           </p>
+
           <button className="bg-main-white rounded-buttonWithArrow-rounded px-buttonWithArrow-padding-x py-buttonWithArrow-padding-y text-main-black ml-buttonWithArrow-margin-left-mobile mr-buttonWithArrow-margin-right-mobile font-medium flex items-center md:ml-buttonWithArrow-margin-left-tablet text-hero-text-size-mobile">
             Czytaj więcej
             <Image
