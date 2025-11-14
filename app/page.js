39b9a6200c757de2f8fd4a2b8ee4ee10d5f3600a
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-14 12:22:16 +0100

    zmienilem hero na lepsze

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
-"use client";
 import Hero from "./homePage/sections/Hero";
 import dynamic from "next/dynamic";
 
-const DynamicAbout = dynamic(() => import("./homePage/sections/About"), {
-  ssr: false,
-});
-const DynamicProjects = dynamic(() => import("./homePage/sections/Projects"), {
-  ssr: false,
-});
+const DynamicAbout = dynamic(() => import("./homePage/sections/About"));
+const DynamicProjects = dynamic(() => import("./homePage/sections/Projects"));
+
+export const metadata = {
+  title: "Sadowski Studio - Nowoczesna Architektura i Design",
+  description:
+    "Odkryj Sadowski Studio - Twoje źródło nowoczesnej architektury i designu. Tworzymy przestrzenie, które inspirują i zachwycają.",
+};
 
 export default function Home() {
   return (
