ab24c128bccb1809491225bb2c331f6543d1c5b3
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-15 11:20:28 +0100

    test

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -2,7 +2,7 @@ import Navigation from "./Navigation";
 
 export default function Header() {
   return (
-    <header className="absolute z-1000 w-full-width top-0">
+    <header className="absolute z-1000 w-full top-0">
       <Navigation />
     </header>
   );
