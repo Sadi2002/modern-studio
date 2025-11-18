3f0745fd84af1b57e8728d1f2e44d266f80c582e
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-18 12:25:25 +0100

    dodanie fonta

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,27 @@
-// "use client";
 import "./globals.css";
 import Header from "./components/Header/Header";
-// import { Poppins } from "next/font/google";
-// import Lenis from "./components/Lenis";
-// import { useEffect, useState } from "react";
+import { Poppins } from "next/font/google";
 
-// export const poppins = Poppins({
-//   subsets: ["latin"],
-//   weight: ["300", "400", "500"],
-//   display: "swap",
-//   preload: false,
-// });
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500"],
+  display: "swap",
+  preload: false,
+});
 
 export default function RootLayout({ children }) {
-  // const [showLenis, setShowLenis] = useState(false);
-
-  // useEffect(() => {
-  //   const timer = setTimeout(() => setShowLenis(true), 10);
-
-  //   return () => clearTimeout(timer);
-  // }, []);
-
   return (
     <html lang="pl">
-      <body className={` min-h-[10000px] bg-bg-main`}>
-        {/* {showLenis && <Lenis />} */}
+      <head>
+        {/* Preconnect do Google Fonts, przyspiesza pobranie fontów */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body className={`${poppins.className} min-h-[10000px] bg-bg-main`}>
         <Header />
         <main>{children}</main>
       </body>
