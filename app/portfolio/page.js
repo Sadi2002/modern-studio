f8c1e856e504bfc0d0b032debc4b44dc7baa93da
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-18 12:47:20 +0100

    dodanie spowrotem portfolio

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,11 +1,11 @@
-// import PortfolioClient from "./PortfolioClient";
+import PortfolioClient from "./PortfolioClient";
 
 export const metadata = {
   title: "Sadowski Studio - Nowoczesna Architektura i Design",
   description:
-    "Odkryj Sadowski Studio - Twoje źródło nowoczesnej architektury i designu. Tworzymy przestrzenie, które inspirują i zachwycają",
+    "Odkryj Sadowski Studio - Twoje źródło nowoczesnej architektury i designu. Tworzymy przestrzenie, które inspirują i zachwycają.",
 };
 
 export default function Portfolio() {
-  return <p>test</p>;
+  return <PortfolioClient />;
 }
