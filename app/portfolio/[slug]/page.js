dcb8f65b9587f1e75a499433f590b5c30f812bcd
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-12-08 19:05:29 +0100

    poprawione

--- app/portfolio/[slug]/page.js
+++ app/portfolio/[slug]/page.js
@@ -70,17 +70,101 @@ export default async function Project({ params }) {
           )}
         </div>
       </div>
+      <div className="pl-0 lg:px-[70px] flex flex-wrap justify-between hidden">
+        <div className="flex flex-col gap-[20px] pb-[40px] w-[48%]">
+          <div className="flex gap-[20px] items-center">
+            <span className="text-[20px]">(01)</span>
+            <span className="text-[23px] font-medium-font-weight">
+              Założenia projektu:
+            </span>
+          </div>
+          <p className="ml-0 lg:ml-[60px] pb-[20px] border-b border-[rgba(0,0,0,0.48)] font-light-font-weight">
+            Projekt willi na Majorce rozpoczął się od określenia potrzeb
+            inwestora, który oczekiwał nowoczesnej, jasnej przestrzeni z
+            widokiem na morze, minimalistycznej formy oraz zastosowania
+            naturalnych materiałów typowych dla śródziemnomorskiego klimatu.
+          </p>
+        </div>
+        <div className="flex flex-col gap-[20px] pb-[40px] w-[48%]">
+          <div className="flex gap-[20px] items-center">
+            <span className="text-[20px]">(01)</span>
+            <span className="text-[23px] font-medium-font-weight">
+              Założenia projektu:
+            </span>
+          </div>
+          <p className="ml-0 lg:ml-[60px] pb-[20px] border-b border-[rgba(0,0,0,0.48)] font-light-font-weight">
+            Projekt willi na Majorce rozpoczął się od określenia potrzeb
+            inwestora, który oczekiwał nowoczesnej, jasnej przestrzeni z
+            widokiem na morze, minimalistycznej formy oraz zastosowania
+            naturalnych materiałów typowych dla śródziemnomorskiego klimatu.
+          </p>
+        </div>
+        <div className="flex flex-col gap-[20px] pb-[40px] w-[48%]">
+          <div className="flex gap-[20px] items-center">
+            <span className="text-[20px]">(01)</span>
+            <span className="text-[23px] font-medium-font-weight">
+              Założenia projektu:
+            </span>
+          </div>
+          <p className="ml-0 lg:ml-[60px] pb-[20px] border-b border-[rgba(0,0,0,0.48)] font-light-font-weight">
+            Projekt willi na Majorce rozpoczął się od określenia potrzeb
+            inwestora, który oczekiwał nowoczesnej, jasnej przestrzeni z
+            widokiem na morze, minimalistycznej formy oraz zastosowania
+            naturalnych materiałów typowych dla śródziemnomorskiego klimatu.
+          </p>
+        </div>
+        <div className="flex flex-col gap-[20px] pb-[40px] w-[48%]">
+          <div className="flex gap-[20px] items-center">
+            <span className="text-[20px]">(01)</span>
+            <span className="text-[23px] font-medium-font-weight">
+              Założenia projektu:
+            </span>
+          </div>
+          <p className="ml-0 lg:ml-[60px] pb-[20px] border-b border-[rgba(0,0,0,0.48)] font-light-font-weight">
+            Projekt willi na Majorce rozpoczął się od określenia potrzeb
+            inwestora, który oczekiwał nowoczesnej, jasnej przestrzeni z
+            widokiem na morze, minimalistycznej formy oraz zastosowania
+            naturalnych materiałów typowych dla śródziemnomorskiego klimatu.
+          </p>
+        </div>
+        <div className="flex flex-col gap-[20px] pb-[40px] w-[48%]">
+          <div className="flex gap-[20px] items-center">
+            <span className="text-[20px]">(01)</span>
+            <span className="text-[23px] font-medium-font-weight">
+              Założenia projektu:
+            </span>
+          </div>
+          <p className="ml-0 lg:ml-[60px] pb-[20px] border-b border-[rgba(0,0,0,0.48)] font-light-font-weight">
+            Projekt willi na Majorce rozpoczął się od określenia potrzeb
+            inwestora, który oczekiwał nowoczesnej, jasnej przestrzeni z
+            widokiem na morze, minimalistycznej formy oraz zastosowania
+            naturalnych materiałów typowych dla śródziemnomorskiego klimatu.
+          </p>
+        </div>
+        <div className="flex flex-col gap-[20px] pb-[40px] w-[48%]">
+          <div className="flex gap-[20px] items-center">
+            <span className="text-[20px]">(01)</span>
+            <span className="text-[23px] font-medium-font-weight">
+              Założenia projektu:
+            </span>
+          </div>
+          <p className="ml-0 lg:ml-[60px] pb-[20px] border-b border-[rgba(0,0,0,0.48)] font-light-font-weight">
+            Projekt willi na Majorce rozpoczął się od określenia potrzeb
+            inwestora, który oczekiwał nowoczesnej, jasnej przestrzeni z
+            widokiem na morze, minimalistycznej formy oraz zastosowania
+            naturalnych materiałów typowych dla śródziemnomorskiego klimatu.
+          </p>
+        </div>
+      </div>
       <div className="px-[20px] md:px-[40px] 2xl:px-[70px]">
-        <Link href={`/portfolio/${slug}/details`}>
-          <button className="font-medium-font-weight text-[clamp(0.75rem,3.5vw,1rem)] relative uppercase after:content-[''] after:bg-main-black after:absolute after:bottom-[-0.5px] after:left-0 after:w-full-width after:h-[1px] after:w-full flex items-center ml-auto">
-            View details{" "}
-            <Image
-              src={arrow}
-              alt="Arrow Icon"
-              className="w-[clamp(1.5rem,3.35vw,1.7rem)] h-[clamp(1.5rem,3.35vw,1.7rem)] top-[0.5px] relative "
-            />
-          </button>
-        </Link>
+        <button className="font-medium-font-weight text-[clamp(0.75rem,3.5vw,1rem)] relative uppercase after:content-[''] after:bg-main-black after:absolute after:bottom-[-0.5px] after:left-0 after:w-full-width after:h-[1px] after:w-full flex items-center ml-auto">
+          View details{" "}
+          <Image
+            src={arrow}
+            alt="Arrow Icon"
+            className="w-[clamp(1.5rem,3.35vw,1.7rem)] h-[clamp(1.5rem,3.35vw,1.7rem)] top-[0.5px] relative "
+          />
+        </button>
       </div>
       <div className="px-[10px] md:px-[20px] 2xl:px-[30px] mt-[10px] md:mt-[16px]">
         <div className="mt-[30px]">
