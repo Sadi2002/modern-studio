70fca5118e3cbdaf9e12d143405455a170e9441a
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-12-09 18:22:15 +0100

    poprawiony post

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -5,6 +5,10 @@ import dataBlog from "@/app/data/dataBlog";
 import { sanityClient } from "../../../lib/sanity/client";
 import { blogPostQuery } from "@/lib/sanity/queries";
 import { urlFor } from "../../../lib/sanity/client";
+import projekt1 from "../../../public/projekt1-large.webp";
+import projekt2 from "../../../public/projekt2-large.webp";
+import projekt3 from "../../../public/projekt3-large.webp";
+import projekt4 from "../../../public/projekt4-large.webp";
 
 export const revalidate = 0;
 
@@ -18,13 +22,25 @@ export async function generateStaticParams() {
 export default async function Post({ params }) {
   const { slug } = await params;
   const posts = await dataBlog();
+  console.log(posts);
 
   const blog = posts.find((post) => post.slug.current === slug);
 
   console.log(blog);
 
+  const getImg = (post, fallback) => {
+    if (post?.imgSrc) {
+      try {
+        return urlFor(post.imgSrc).url();
+      } catch {
+        return fallback;
+      }
+    }
+    return fallback;
+  };
+
   return (
-    <section className="pt-[100px] px-[20px] md:px-[40px] lg:px-[50px] md:pt-[150px] lg:pt-[150px] 2xl:pt-[200px] w-full mb-[40px] lg:mb-[80px] relative">
+    <section className="pt-[100px] px-[20px] md:px-[40px] lg:px-[50px] md:pt-[150px] lg:pt-[150px] 2xl:pt-[200px] w-full mb-[80px] lg:mb-[80px] relative">
       <div>
         <div>
           <div className=" flex flex-col lg:flex-row justify-between lg:items-end mb-[20px] md:mb-[40px] lg:mb-[0px] gap-[20px]">
@@ -92,58 +108,73 @@ export default async function Post({ params }) {
             />
           </Link>
         </div>
-        <div className="hidden lg:flex lg:justify-between">
+        <div className="flex flex-col justify-between lg:flex-row">
           {/* 1 kolumna */}
-          <div className="w-[39%] inline-block">
-            <div className="lg:aspect-[8/8] relative xl:aspect-[8/7]">
-              <Image
-                src="/about-large.webp"
-                alt="pokoj"
-                fill
-                className="object-cover"
-              />
-            </div>
-            <div className="flex flex-col gap-[8px] justify-between mt-[10px] w-full text-[clamp(12px,3.35vw,1rem)] font-normal-font-weight">
-              <span className="font-medium-font-weight max-w-[65%]">
-                Designing a Luxury Mediterranean Villa
-              </span>
-              <span className="text-[#757575] text-[14px]">2025</span>
-            </div>
+
+          <div className="w-full mb-[50px] lg:mb-[0] lg:w-[39%] inline-block">
+            <Link href={`${posts[0]?.slug.current}`}>
+              <div className="max-w-[80%] aspect-[8/7] lg:aspect-[8/8] lg:max-w-[100%] relative xl:aspect-[8/7]">
+                <Image
+                  src={posts[0] ? getImg(posts[0], projekt3) : projekt3}
+                  alt={posts[0]?.alt || posts[0]?.title || "pokoj"}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <div className="flex flex-col gap-[8px] justify-between mt-[10px] w-full text-[clamp(12px,3.35vw,1rem)] font-normal-font-weight">
+                <span className="font-medium-font-weight max-w-[65%]">
+                  {posts[0]?.title || "Designing a Luxury Mediterranean Villa"}
+                </span>
+                <span className="text-[#757575] text-[14px]">
+                  {posts[0]?.date || "March 2025"}
+                </span>
+              </div>
+            </Link>
           </div>
 
           {/* 2 kolumna */}
-          <div className="w-[30%] inline-block">
-            <div className="lg:aspect-[8/5] relative ">
-              <Image
-                src="/about-large.webp"
-                alt="pokoj"
-                fill
-                className="object-cover"
-              />
-            </div>
+          <div className="w-full mb-[50px] max-w-[86%] ml-auto lg:ml-0 flex lg:max-w-[100%] lg:mb-[0] lg:w-[30%] inline-block">
+            <Link href={`${posts[1]?.slug.current}`}>
+              <div className="flex lg:block aspect-[5/3]  lg:aspect-[8/5] relative ">
+                <Image
+                  src={posts[1] ? getImg(posts[1], projekt2) : projekt2}
+                  alt={posts[1]?.alt || posts[1]?.title || "pokoj"}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+            </Link>
             <div className="flex flex-col gap-[8px] justify-between mt-[10px] w-full text-[clamp(12px,3.35vw,1rem)] font-normal-font-weight">
               <span className="font-medium-font-weight max-w-[70%]">
-                Designing a Luxury Mediterranean Villa
+                {posts[1]?.title ||
+                  "Maximizing Light and Views in Your Mallorca Home"}
               </span>
-              <span className="text-[#757575] text-[14px]">2025</span>
+              <span className="text-[#757575] text-[14px]">
+                {posts[1]?.date || "January 2025"}
+              </span>
             </div>
           </div>
 
           {/* 3 kolumna */}
-          <div className="w-[20%] inline-block xl:w-[20%] ">
-            <div className="lg:aspect-[6/8] xl:aspect-[6/8] relative ] ">
-              <Image
-                src="/about-large.webp"
-                alt="pokoj"
-                fill
-                className="object-cover"
-              />
-            </div>
+          <div className="max-w-[78%] lg:max-w-[100%] lg:w-[20%] inline-block xl:w-[20%] ">
+            <Link href={`${posts[2]?.slug.current}`}>
+              <div className="aspect-[7/8]  lg:aspect-[6/8] xl:aspect-[6/8] relative ">
+                <Image
+                  src={posts[2] ? getImg(posts[2], projekt4) : projekt4}
+                  alt={posts[2]?.alt || posts[2]?.title || "pokoj"}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+            </Link>
             <div className="flex flex-col gap-[8px] justify-between mt-[10px] w-full text-[clamp(12px,3.35vw,1rem)] font-normal-font-weight">
               <span className="font-medium-font-weight max-w-[85%]">
-                Designing a Luxury Mediterranean Villa
+                {posts[2]?.title ||
+                  "Materials and Finishes Inspired by Mallorca."}
               </span>
-              <span className="text-[#757575] text-[14px]">2025</span>
+              <span className="text-[#757575] text-[14px]">
+                {posts[2]?.date || "August 2024"}
+              </span>
             </div>
           </div>
         </div>
