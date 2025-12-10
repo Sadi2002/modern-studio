b25454182a50d63b103e39a8cad4f53d55caa2b2
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-12-10 16:19:58 +0100

    zrobione portfolio z sanity

--- app/data/dataProjects.js
+++ app/data/dataProjects.js
@@ -1,11 +1,14 @@
 import { sanityClient } from "../../lib/sanity/client";
-import { homePageQuery } from "../../lib/sanity/queries";
+import { homePageQuery, portfolioPageQuery } from "../../lib/sanity/queries";
 
 export default async function dataProjects() {
   const homePageData = await sanityClient.fetch(homePageQuery);
   const { projectsSection } = homePageData;
 
-  const projects = [...projectsSection.projects];
+  const portfolioPageData = await sanityClient.fetch(portfolioPageQuery);
+  const { portfolioSection } = portfolioPageData;
+
+  const projects = [...projectsSection.projects, ...portfolioSection.projects];
 
   return projects;
 }
