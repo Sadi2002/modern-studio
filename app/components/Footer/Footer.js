e50400b62ed6d9c9adec9cd62d62e0f9f67e0c25
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-12-11 11:55:10 +0100

    polaczone cale sanity ze strona

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,61 +1,44 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Footer() {
+import { sanityClient } from "../../../lib/sanity/client";
+import { footerQuery } from "@/lib/sanity/queries";
+
+import { urlFor } from "../../../lib/sanity/client";
+
+export const revalidate = 0;
+
+export default async function Footer() {
+  const footerData = await sanityClient.fetch(footerQuery);
+  const footer = footerData;
+  console.log(footer);
   return (
     <footer className="bg-main-black pb-[30px]">
       <div className="lg:flex ">
         <div className="flex justify-center mb-[5px] lg:w-[100%] lg:pt-0">
           <Image
-            src="/footer.png"
+            src={urlFor(footer.logo).url()}
             alt="footer"
             width={220}
             height={165}
             className="object-cover md:w-[400px]"
           />
         </div>
-        <div className="border-t border-[rgba(255,255,255,.2)] w-[70%] mb-[70px] lg:pt-[80px] lg:mb-0 lg:border-none">
+        <div className="border-t border-[rgba(255,255,255,.2)] w-[70%] mb-[70px] lg:pt-[80px] lg:mb-0 lg:border-none ">
           <ul>
-            <li className="py-[15px] border-b border-[rgba(255,255,255,.2)] lg:py-[30px]">
-              <Link
-                href="/about"
-                className="uppercase text-white text-[clamp(20px,5vw,23px)] flex pl-[20px] md:pl-[40px]"
+            {footer.navLinks.map((link, index) => (
+              <li
+                key={index}
+                className="py-[15px] border-b border-[rgba(255,255,255,.2)]  lg:py-[30px] last:lg:border-b-0"
               >
-                About us
-              </Link>
-            </li>
-            <li className="py-[15px] border-b border-[rgba(255,255,255,.2)] lg:py-[30px]">
-              <Link
-                href="/portfolio"
-                className="uppercase text-white text-[clamp(20px,5vw,23px)] flex pl-[20px] md:pl-[40px]"
-              >
-                Portfolio
-              </Link>
-            </li>
-            <li className="py-[15px] border-b border-[rgba(255,255,255,.2)] lg:py-[30px]">
-              <Link
-                href="/process"
-                className="uppercase text-white text-[clamp(20px,5vw,23px)] flex pl-[20px] md:pl-[40px]"
-              >
-                Process
-              </Link>
-            </li>
-            <li className="py-[15px] border-b border-[rgba(255,255,255,.2)] lg:py-[30px]">
-              <Link
-                href="#"
-                className="uppercase text-white text-[clamp(20px,5vw,23px)] flex pl-[20px] md:pl-[40px]"
-              >
-                Blog
-              </Link>
-            </li>
-            <li className="py-[15px] border-b border-[rgba(255,255,255,.2)] lg:border-none lg:py-[30px]">
-              <Link
-                href="/contact"
-                className="uppercase text-white text-[clamp(20px,5vw,23px)] flex pl-[20px] md:pl-[40px]"
-              >
-                Contact
-              </Link>
-            </li>
+                <Link
+                  href={link.url}
+                  className="uppercase text-white text-[clamp(20px,5vw,23px)] flex pl-[20px] md:pl-[40px]"
+                >
+                  {link.title}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
@@ -64,83 +47,35 @@ export default function Footer() {
        xl:justify-between "
       >
         <div className="flex flex-wrap justify-between min-[436px]:gap-x-[50px] gap-y-[50px] lg:mx-[50px] lg:pt-[40px] lg:max-w-[50%] xl:justify-between 2xl:max-w-[45%]">
-          <div>
-            <h3 className="text-white text-[clamp(20px,5.5vw,23px)] font-medium mb-[20px]">
-              Quick contact
-            </h3>
-            <ul className="flex flex-col gap-[5px]">
-              <li>
-                <Link
-                  href="#"
-                  className="text-white text-[clamp(12px,3.5vw,16px)] flex"
-                >
-                  +48 512 381 126
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="#"
-                  className="text-white text-[clamp(12px,3.5vw,16px)] flex"
-                >
-                  Contact@gmail.com
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="text-white text-[clamp(20px,5.5vw,23px)] font-medium mb-[20px]">
-              Information
-            </h3>
-            <ul className="flex flex-col gap-[5px]">
-              <li>
-                <Link
-                  href="#"
-                  className="text-white text-[clamp(12px,3.5vw,16px)] flex"
-                >
-                  +48 124 151 123
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="#"
-                  className="text-white text-[clamp(12px,3.5vw,16px)] flex"
-                >
-                  kontat@wp.com
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="text-white text-[clamp(20px,5.5vw,23px)] font-medium mb-[20px]">
-              SociaI media
-            </h3>
-            <ul className="flex flex-col gap-[5px]">
-              <li>
-                <Link
-                  href="#"
-                  className="text-white text-[clamp(12px,3.5vw,16px)] flex"
-                >
-                  Facebook
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="#"
-                  className="text-white text-[clamp(12px,3.5vw,16px)] flex"
-                >
-                  Instagram
-                </Link>
-              </li>
-            </ul>
-          </div>
+          {footer.quickContact.map((contact, index) => (
+            <div key={index}>
+              <h3 className="text-white text-[clamp(20px,5.5vw,23px)] font-medium mb-[20px]">
+                {contact.sectionTitle}
+              </h3>
+              <ul className="flex flex-col gap-[5px]">
+                {contact.items.map((item, idx) => (
+                  <li key={idx}>
+                    <Link
+                      href={item.url}
+                      className="text-white text-[clamp(12px,3.5vw,16px)] flex"
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
       <div className="flex flex-col gap-[5px] items-end mx-margin-mobile md:mx-tablet lg:flex-row lg:justify-end lg:gap-x-[50px] 2xl:gap-x-[80px]">
+        <Link href={footer.privacyPolicyHref}>
+          <span className="text-[clamp(12px,3.5vw,16px)]   text-white">
+            {footer.privacyPolicyLabel}
+          </span>
+        </Link>
         <span className="text-[clamp(12px,3.5vw,16px)]   text-white">
-          Polityka prwatności
-        </span>
-        <span className="text-[clamp(12px,3.5vw,16px)]   text-white">
-          2025 wszelkie prawa zastrzeżone
+          {footer.copyrightText}
         </span>
       </div>
     </footer>
