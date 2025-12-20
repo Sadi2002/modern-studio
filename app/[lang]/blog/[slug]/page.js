c49edec55499a103bb4b250d182d9ad786173459
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-12-20 11:27:58 +0100

    dodany przycisk na bloga

--- app/[lang]/blog/[slug]/page.js
+++ app/[lang]/blog/[slug]/page.js
@@ -101,7 +101,7 @@ export default async function Post({ params }) {
         <h3 className="text-[clamp(1.5rem,8vw,3rem)] leading-[clamp(2.2rem,10vw,3.5rem)] font-medium mb-[20px] xl:mb-[20px] max-w-[500px] lg:text-[45px] lg:leading-[45 px] lg:max-w-[500px] lg:w-[100%] xl:text-[60px] xl:leading-[60px] xl:max-w-[600px] 2xl:max-w-[1200px] 2xl:font-normal 2xl:text-[80px] 2xl:leading-[80px] 2xl:w-[850px] uppercase">
           SEE OTHER BLOGS
         </h3>
-        <div className="flex justify-end">
+        <div className="hidden lg:flex justify-end">
           <Link
             href={`/${lang}/blog`}
             className="bg-main-black mb-[40px] rounded-[500px] px-[clamp(1rem,3.35vw,1.5rem)] py-[clamp(0.5rem,3.35vw,0.7rem)] text-main-white ml-auto mr-0 font-medium flex items-center md:ml-0 text-[clamp(0.75rem,3.35vw,1rem)] md:flex md:max-h-[50px] self-end"
@@ -114,7 +114,7 @@ export default async function Post({ params }) {
             />
           </Link>
         </div>
-        <div className="flex flex-col justify-between lg:flex-row">
+        <div className="flex flex-col justify-between lg:flex-row mb-[40px] lg:mb-0">
           {/* 1 kolumna */}
 
           <div className="w-full mb-[50px] max-w-[80%] lg:max-w-[100%] lg:mb-[0] lg:w-[39%] inline-block">
@@ -190,6 +190,19 @@ export default async function Post({ params }) {
               </span>
             </div>
           </div>
+        </div>
+        <div className="flex lg:hidden justify-end">
+          <Link
+            href={`/${lang}/blog`}
+            className="bg-main-black rounded-[500px] px-[clamp(1rem,3.35vw,1.5rem)] py-[clamp(0.5rem,3.35vw,0.7rem)] text-main-white ml-auto mr-0 font-medium flex items-center md:ml-0 text-[clamp(0.75rem,3.35vw,1rem)] md:flex md:max-h-[50px] self-end"
+          >
+            Zobacz wszystkie
+            <Image
+              src={ArrowWhite}
+              alt="strzałka"
+              className="w-[clamp(1.5rem,3.35vw,1.7rem)] h-[clamp(1.5rem,3.35vw,1.7rem)] top-[0.5px] relative "
+            />
+          </Link>
         </div>
       </div>
     </section>
