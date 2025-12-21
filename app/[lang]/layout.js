b3fe34b3d98b98de7143bc5f19b3d4c16ca45649
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-12-21 13:03:23 +0100

    poprawiony poppins

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -2,25 +2,18 @@ import "../globals.css";
 
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import { Poppins } from "next/font/google";
-
-export const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["300", "400", "500"],
-  display: "swap",
-});
 
 export default async function RootLayout({ children, params }) {
   const getParams = await params;
   const lang = getParams?.lang || "en";
 
   return (
-    <div className={poppins.className}>
+    <>
       <Header lang={lang} />
 
       <main>{children}</main>
 
       <Footer lang={lang} />
-    </div>
+    </>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,18 @@
 import { ViewTransitions } from "next-view-transitions";
 import "./globals.css";
+import { Poppins } from "next/font/google";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500"],
+  display: "swap",
+});
 
 export default function RootLayout({ children }) {
   return (
     <ViewTransitions>
       <html lang="en">
-        <body>{children}</body>
+        <body className={poppins.className}>{children}</body>
       </html>
     </ViewTransitions>
   );
