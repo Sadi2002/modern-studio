c62bc7ef3c0252b8401a6180357128117ec7391a
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2026-01-02 20:46:06 +0100

    dodane do prcoess dodatkowa sekcja

--- app/[lang]/process/Delivers.js
+++ app/[lang]/process/Delivers.js
@@ -7,8 +7,8 @@ export default function Delivers() {
   return (
     <section className="lg:border-t lg:border-[rgba(0,0,0,0.2)] pt-[40px] lg:pt-[100px] w-screen mb-[80px] md:mb-[80px] lg:mb-[80px] xl:mb-[150px] relative flex justify-between">
       <div className="w-full lg:flex lg:justify-between lg:items-center lg:w-1/2">
-        <div className="w-full px-[20px] md:px-[40px] lg:px-[50px] lg:max-w-[700px] 2xl:max-w-[800px] 2xl:pl-[50px]">
-          <h1 className="text-[clamp(1.5rem,8vw,3rem)] leading-[clamp(2.2rem,10vw,3.5rem)] uppercase font-medium mb-[20px] xl:mb-[20px] max-w-[500px] lg:text-[45px] lg:leading-[45px] lg:max-w-[500px] lg:w-[100%] xl:text-[60px] xl:leading-[60px] xl:max-w-[600px] 2xl:max-w-[1200px] lg:font-normal 2xl:text-[clamp(60px,4.3vw,5rem)] 2xl:leading-[80px] 2xl:w-[750px] min-[1589px]:!w-[800px] ">
+        <div className="w-full px-[20px] md:px-[40px] lg:pl-[50px] lg:max-w-[700px] 2xl:max-w-[800px] 2xl:pl-[50px]">
+          <h1 className="text-[clamp(1.5rem,8vw,3rem)] leading-[clamp(2.2rem,10vw,3.5rem)] uppercase font-medium mb-[20px] xl:mb-[20px] max-w-[500px] lg:text-[45px] lg:leading-[45px] lg:max-w-[600px] lg:w-[100%] xl:text-[60px] xl:leading-[60px] xl:max-w-[600px] 2xl:max-w-[1200px] lg:font-normal 2xl:text-[clamp(60px,4.3vw,5rem)] 2xl:leading-[80px] 2xl:w-[750px] min-[1589px]:!w-[800px] ">
             <RevealAfterTransition delay={0} stagger={50}>
               What This Process Delivers
             </RevealAfterTransition>
@@ -36,8 +36,8 @@ export default function Delivers() {
           </div>
         </div>
       </div>
-      <div className="w-[60%] lg:flex gap-[20px] px-[20px] md:px-[40px] lg:px-[50px] hidden">
-        <div className="w-[35%] h-[200px] relative aspect-8/7 overflow-hidden self-end">
+      <div className="w-[60%] lg:w-[50%] lg:flex lg:items-end gap-[20px] px-[20px] md:px-[40px] lg:px-[50px] hidden">
+        <div className="w-[35%] h-[200px] lg:h-[100px] xl:h-[120px] 2xl:h-[200px] lg:w-[40%] relative aspect-8/7 overflow-hidden self-end">
           <Image
             className="object-cover"
             fill
@@ -45,7 +45,7 @@ export default function Delivers() {
             alt="test"
           />
         </div>
-        <div className="w-[75%] relative aspect-8/8 overflow-hidden">
+        <div className="w-[75%] lg:w-[60%] lg:h-[75%] xl:w-[85%] 2xl:w-[75%] xl:h-[100%] relative aspect-8/8 overflow-hidden">
           <Image
             className="object-cover"
             fill
