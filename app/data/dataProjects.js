645b5fe0b7fa5e2cce0b27a225f07a9d3ad91318
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2026-01-04 15:46:09 +0100

    test

--- app/data/dataProjects.js
+++ app/data/dataProjects.js
@@ -1,25 +1,11 @@
-import { sanityClient } from "../../lib/sanity/client";
-import { homePageQuery, portfolioPageQuery } from "../../lib/sanity/queries";
 import { projectsSectionData } from "../data/sectionsData/homePage/projectsSectionData";
 import { portfolioSectionData } from "../data/sectionsData/portfolioPage/portfolioSectionData";
 
-console.log(portfolioSectionData);
+export default function dataProjects() {
+  const projects = [
+    ...(projectsSectionData?.projects || []),
+    ...(portfolioSectionData?.projects || []),
+  ];
 
-export default async function dataProjects() {
-  try {
-    const [homePageData, portfolioPageData] = await Promise.all([
-      sanityClient.fetch(homePageQuery),
-      sanityClient.fetch(portfolioPageQuery),
-    ]);
-
-    const projects = [
-      ...(projectsSectionData?.projects || []),
-      ...(portfolioSectionData?.projects || []),
-    ];
-
-    return projects;
-  } catch (error) {
-    console.error("Error", error);
-    return [];
-  }
+  return projects;
 }
