5462f9ff5c7cc2ca15496470958971b3242d9a4b
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2026-01-05 10:09:26 +0100

    test

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,13 @@
 import { ViewTransitions } from "next-view-transitions";
 import "./globals.css";
-import Intro from "./components/Intro";
+// import Intro from "./components/Intro";
 
 export default function RootLayout({ children }) {
   return (
     <ViewTransitions>
       <html lang="en">
         <body className="max-w-[1920px] mx-auto">
-          <Intro />
+          {/* <Intro /> */}
           {children}
         </body>
       </html>
