b6b180b05b44a7429ef70780577873318454cd22
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2026-01-25 15:23:43 +0100

    dodany GA

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
 import { ViewTransitions } from "next-view-transitions";
 import "./globals.css";
 import Intro from "./components/Intro";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function RootLayout({ children }) {
   return (
     <ViewTransitions>
       <html lang="en">
         <body className="max-w-[1920px] mx-auto">
+          <GoogleAnalytics gaId="G-QRXY895P90" />
           <Intro />
           {children}
         </body>
